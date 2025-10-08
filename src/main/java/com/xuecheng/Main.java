834307c772b05e2c4c4fae4c56d81295606ce1b7
yx <2181025743@qq.com>
2025-10-08 18:20:20 +0800

    角色2第二次修改

--- src/main/java/com/xuecheng/Main.java
+++ src/main/java/com/xuecheng/Main.java
@@ -3,7 +3,9 @@ package com.xuecheng;
 public class Main {
     public static void main(String[] args) {
         // 角色2第一次修改
-        System.out.printf("HelloWorld!");
+        System.out.println("HelloWorld!");
         // 角色1第二次修改
+        // 处理冲突
+        System.out.println("角色1打印：BBBBB");
     }
 }
